ff33e040059e27622d84633df8a8d2defb40964b
timetzhang <367271@qq.com>
2017-09-20 11:23:14 +0800

    first commit

---
+++ CoinMindReader/FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CoinMindReader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(868, 659);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(103)))), ((int)(((byte)(211)))));
+            this.ClientSize = new System.Drawing.Size(868, 659);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Name = "FormMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "CODEA";
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+    }
+}
+
---
+++ CoinMindReader/FormMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CoinMindReader
+{
+    public partial class FormMain : Form
+    {
+        int[,] num = new int[6, 6];
+
+        public FormMain()
+        {
+            InitializeComponent();
+            GenerateCoin();
+        }
+
+        public void GenerateCoin()
+        {
+            this.flowLayoutPanel.Controls.Clear();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                int verify = 0;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    num[i, j] = rnd.Next(0, 2);
+                    if (num[i, j] == 0)
+                    {
+                        verify++;
+                    }
+                    num[i, 5] = verify%2;
+                }
+            }
+
+            for (int i = 0; i <= 5; i++)
+            {
+                int verify = 0;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    if (num[j, i] == 0)
+                    {
+                        verify++;
+                    }
+                    num[5, i] = verify % 2;
+                }
+            }
+
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    Label l = new Label();
+                    l.Text = num[i, j].ToString();
+                    l.ForeColor = Color.White;
+                    l.TextAlign = ContentAlignment.MiddleCenter;
+                    l.Font = new Font("Verdana", this.flowLayoutPanel.Height/12, FontStyle.Bold);
+                    l.Size = new Size(this.flowLayoutPanel.Width/6-6, this.flowLayoutPanel.Height/6-6);
+                    l.Click += new System.EventHandler(this.l_Click);
+                    this.flowLayoutPanel.Controls.Add(l);
+                }
+            }
+            Button b = new Button();
+            b.Text = "产生新随机阵列";
+            b.Font = new Font("Verdana", 10, FontStyle.Bold);
+            b.BackColor = Color.White;
+            b.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(103)))), ((int)(((byte)(211)))));
+            b.Height = 35;
+            b.Width = this.flowLayoutPanel.Width-6;
+            b.Click += new System.EventHandler(this.b_Click);
+            this.flowLayoutPanel.Controls.Add(b);
+        }
+
+        private void l_Click(object sender, EventArgs e)
+        {
+            ((Label)sender).Text = ((Label)sender).Text == "0" ? "1" : "0";
+        }
+
+        private void b_Click(object sender, EventArgs e) {
+            GenerateCoin();
+        }
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            GenerateCoin();
+        }
+    }
+}
